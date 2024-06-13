b29b60a81f5c64f0350ab63c32c28ec8bfafb774
nerdskill <alexeysiver@gmail.com>
2024-06-14 01:34:23 +0300

    client-state popup task done

---
+++ client-state/popup/task.js
@@ -0,0 +1,32 @@
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+const modal = document.getElementById('subscribe-modal');
+const modalClose = document.querySelector('.modal__close');
+
+if (!getCookie('modalClosed')) {
+    modal.classList.add('modal_active');
+}
+
+modalClose.addEventListener('click', function() {
+    modal.classList.remove('modal_active');
+    setCookie('modalClosed', true, 30); 
+});
